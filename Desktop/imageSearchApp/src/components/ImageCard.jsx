422bc780434a3b6ef24f6b7674928ac718352186
w3School_test <neetumorbca@gmail.com>
2023-07-24 16:41:02 +0530

    done

---
+++ Desktop/imageSearchApp/src/components/ImageCard.jsx
@@ -0,0 +1,20 @@
+// ImageCard.js
+import React from 'react';
+import ImagePopup from './ImagePopup';
+
+const ImageCard = ({ image }) => {
+  const [isPopupOpen, setPopupOpen] = React.useState(false);
+
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
+
+  return (
+    <div className="image-card" onClick={togglePopup}>
+      <img src={image.url} alt={image.alt} />
+      {isPopupOpen && <ImagePopup image={image} onClose={togglePopup} />}
+    </div>
+  );
+};
+
+export default ImageCard;
